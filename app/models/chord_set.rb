0c504f51c278c5b091e2bf7c69475d1e082f69cf
Eugene Koontz <ekoontz@hiro-tan.org>
2010-02-28 14:01:03 -0800

    showing chords for this set

--- app/models/chord_set.rb
+++ app/models/chord_set.rb
@@ -3,13 +3,20 @@ class ChordSet < ActiveRecord::Base
 
   #class instance : export all chords (that satisfy specified conditions, or all, if no conditions given).
   def ChordSet::export(xml,conditions = "")
+
+
     xml.chord_sets(:time => Time.now) {
       for chord_set in find(:all,:order=>"name ASC",:conditions=>conditions)
         chord_set.export(xml)
       end
 
+      # find all chords for which there is a corresponding 
+      # chord_set_member.
       xml.chords {
-        for chord in Chord::find(:all,:order=>"name ASC")
+        for chord in Chord::find_by_sql ["SELECT * FROM chords 
+                                                  WHERE id IN (SELECT chord_id 
+                                                                 FROM chord_set_members 
+                                                                WHERE chord_set_id = ?)",chord_set[:id]]
           chord.export(xml)
         end
       }
